ff7b2309ddc4ca9983c5d2a728a2c95824d3dfb3
gustavosilvaf <gustavofsilva@email.com>
2021-07-10 13:37:47 -0300

    Componentizing application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: Array<GenreResponseProps>,
+  selectedGenreId: number,
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>,
+}
+
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
